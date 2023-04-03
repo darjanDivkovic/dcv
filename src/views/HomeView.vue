5651da12241f32c02cf1cd441ff67777ee94b6c9
Darjan <daarjan.tz@gmail.com>
2023-04-03 17:57:14 +0200

    Handle appear anims on home and schedule a talk

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
     <div class="content">
-      <h1>It’s time to implement</br> that web solution!</h1>
-      <p>Web developer providing <span class="purple-span">design-to-deployment services</span>,</br>
+      <div class="header-container">
+        <h1 class="header">It’s time to implement</br> that web solution!</h1>
+      </div>
+      <div class="text-container">
+        <p class="text">Web developer providing <span class="purple-span">design-to-deployment services</span>,</br>
          building <span class="grey-span">SaaS apps</span> & <span class="grey-span">other web products</span>,</br>
          specializing in delivering pixel-perfect Front End!</p>
+      </div>
+      
       <div id="explore-btn">
-        <DButton text="Explore further"/>
+        <DButton class="d-btn" text="Explore further"/>
       </div>
     </div>
   </div>
@@ -22,7 +27,9 @@ export default {
     DButton,
   },
   mounted() {
-    gsap.fromTo('.home', {opacity: 0}, {opacity: 1, duration: 0.7, delay: 1})
+    gsap.to('.header', {opacity: 1, delay: 1, duration: 0.7, y: 0})
+    gsap.to('.text', {opacity: 1, delay: 1.6, duration: 0.5, y: 0})
+    gsap.to('.d-btn', {opacity: 1, delay: 2, duration: 0.3, y: 0})
   }
 }
 </script>
@@ -39,16 +46,15 @@ export default {
   flex-direction: column
   justify-content: center
   align-items: center 
-  & > h1
-    text-align: center
-    background: -webkit-linear-gradient(30deg, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0.4) 100%)
-    -webkit-background-clip: text
-    -webkit-text-fill-color: transparent
-    font-size: 75px
-    line-height: 100px
+
+.text-container
+  overflow: hidden
+
   & > p
     text-align: center
     font-size: 20px
+    opacity: 0
+    transform: translateY(120%)
     line-height: 30px
     color: rgba(255, 255, 255, 0.3)
     & > .purple-span
@@ -56,6 +62,23 @@ export default {
       font-weight: 600
     & > .grey-span
       font-weight: 600
+
+.header-container
+  overflow: hidden
+.header
+  text-align: center
+  opacity: 0
+  background: -webkit-linear-gradient(30deg, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0.4) 100%)
+  -webkit-background-clip: text
+  -webkit-text-fill-color: transparent
+  font-size: 75px
+  transform: translateY(-120%)
+  line-height: 100px
+  
 #explore-btn
   margin-top: 20px
+  overflow: hidden
+  & > button
+    opacity: 0
+    transform: translateY(100%)
 </style>
--- src/views/ScheduleATalkView.vue
+++ src/views/ScheduleATalkView.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="schedule-a-talk">
+  <div class="home">
     <div class="content">
-      <h1>Ready to collaborate & upgrade<br/> your solution?</h1>
-      <p>
-        <span class="grey-span">Nothing to it but to do it!</span><br/>
-        Schedule a 1:1 <span class="grey-span">Google Meet</span> w <span class="grey-span">@Darjan</span></br>
-         and discuss your project's needs...</p>
+      <div class="header-container">
+        <h1 class="header">Ready to collaborate & upgrade<br/> your solution?</h1>
+      </div>
+      <div class="text-container">
+        <p class="text"><span class="grey-span">Nothing to it but to do it!</span><br/>
+        Schedule a 1:1 <span class="green-span">Google Meet w @Darjan</span><br/>
+        and discuss your project's needs...</p>
+      </div>
+      
       <div id="explore-btn">
-        <DButton text="Meet Me"/>
+        <DButton class="d-btn" text="Meet Me"/>
+      </div>
+      <div class="calendly-container">
+        <p class="calendly">Powered by <span class="grey-span">Calendly</span></p>
       </div>
     </div>
   </div>
@@ -23,13 +30,16 @@ export default {
     DButton,
   },
   mounted() {
-    gsap.fromTo('.schedule-a-talk', {opacity: 0}, {opacity: 1, duration: 0.7, delay: 1})
+    gsap.to('.header', {opacity: 1, delay: 1, duration: 0.7, y: 0})
+    gsap.to('.text', {opacity: 1, delay: 1.6, duration: 0.5, y: 0})
+    gsap.to('.d-btn', {opacity: 1, delay: 2, duration: 0.3, y: 0})
+    gsap.to('.calendly', {opacity: 0.5533, delay: 2.5, duration: 0.2, y: 0})
   }
 }
 </script>
 
 <style lang="sass" scoped>
-.schedule-a-talk
+.home
   height: 100%
   display: flex
   justify-content: center
@@ -40,23 +50,44 @@ export default {
   flex-direction: column
   justify-content: center
   align-items: center 
-  & > h1
-    text-align: center
-    background: -webkit-linear-gradient(30deg, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0.4) 100%)
-    -webkit-background-clip: text
-    -webkit-text-fill-color: transparent
-    font-size: 75px
-    line-height: 100px
+
+.text-container
+  overflow: hidden
+
   & > p
     text-align: center
     font-size: 20px
+    opacity: 0
+    transform: translateY(120%)
     line-height: 30px
     color: rgba(255, 255, 255, 0.3)
-    & > .purple-span
-      color: #6639ED
-      font-weight: 600
-    & > .grey-span
-      font-weight: 600
+.green-span
+  color: rgba(0, 228, 192, 0.6)
+  font-weight: 600
+.grey-span
+  font-weight: 600
+
+.header-container
+  overflow: hidden
+.header
+  text-align: center
+  opacity: 0
+  background: -webkit-linear-gradient(30deg, rgba(255,255,255,0.8) 0%, rgba(255,255,255,0.4) 100%)
+  -webkit-background-clip: text
+  -webkit-text-fill-color: transparent
+  font-size: 75px
+  transform: translateY(-120%)
+  line-height: 100px
+  
 #explore-btn
   margin-top: 20px
+  overflow: hidden
+  & > button
+    opacity: 0
+    transform: translateY(100%)
+
+.calendly
+  opacity: 0
+  font-size: 10px
+  color: #6C6464
 </style>
