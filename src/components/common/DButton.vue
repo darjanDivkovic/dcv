70e7cc91768e8fabe6d48abdb62b9b0a814eb240
Darjan <daarjan.tz@gmail.com>
2023-04-05 20:09:33 +0200

    p

--- src/components/common/DButton.vue
+++ src/components/common/DButton.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="d-button">
-        <p>{{ text }}</p>
-        <img class="d-button-img" src="@/assets/small_links/handshake.svg" alt="">
-    </div>
+  <div class="d-button">
+    <p>{{ text }}</p>
+  </div>
 </template>
 
 <script>
@@ -14,42 +13,32 @@ export default {
 
 <style lang="sass" scoped>
 .d-button
-    font-size: 2vh !important
-    font-family: primary
-    font-weight: 400
-    display: flex
-    opacity: 0
-    align-items: center 
-    line-height: 0
-    color: #fff
-    padding: 5px 10px !important
-    background: none
-    border: 1px solid rgba(86, 81, 81, 0.1)
-    border-radius: 5px
-    cursor: pointer
-    transition: all 100ms
-    & > p
-        width: max-content !important
+  color: #fff
+  border: 1px solid rgba(86, 81, 81, 0.1)
+  display: flex
+  justify-items: center
+  align-items: center
+  border-radius: 10px
+  flex-direction: row
+  & > p 
+    margin-right: 10px
     
-.d-button-img
-    height: 18px
-    margin-left: 5px
-
-@media (max-width: 965px)
-    .d-button
-        padding: 3px 8px
-        border-radius: 8px
-        margin-top: 5px
-        min-width: 60px
-        max-height: 30px
-    .d-button-img
-        height: 12px
-
-@media (max-width: 520px)
-    .d-button   
-        padding: 15px 10px
-        display: flex
-        flex-direction: row
-
+    padding: 0 4.5vh 0 12px
+    font-size: 2vh !important
+    margin: 0
+    position: relative
+    width: max-content
+    &::after
+      content: ''
+      position: absolute
+      background-image: url('@/assets/small_links/handshake.svg')
+      background-position: center
+      background-repeat: no-repeat
+      background-size: contain
+      min-height: 2vh
+      min-width: 2vh
+      top: 2px
+      right: 10px
+        
 </style>
 
